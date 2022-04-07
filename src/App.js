93457292928cf1b9049f4969dbd66bc4332752b0
neha-keshri <nehakeshri.sunshine@gmail.com>
2022-04-07 20:24:57 +0530

    remove hash

--- src/App.js
+++ src/App.js
@@ -1,8 +1,7 @@
-import { useState } from 'react';
-import './App.css';
+import { useState } from "react";
+import "./App.css";
 
 function App() {
-
   const [checked, setChecked] = useState([]);
   let array = Array.from({ length: 16 }, () => 0);
 
@@ -14,7 +13,9 @@ function App() {
             {array?.map((_, index) => (
               <div
                 className="col-3"
-                style={{ backgroundColor: checked.includes(index) ? "red" : "blue" }}
+                style={{
+                  backgroundColor: checked.includes(index) ? "red" : "blue",
+                }}
                 onClick={() => {
                   let temp = [...checked];
                   if (temp.length === 2) {
@@ -22,7 +23,9 @@ function App() {
                   }
                   if (!temp.includes(index)) setChecked([...temp, index]);
                 }}
-              ><div>Box#{index + 1}</div></div>
+              >
+                <div>Box {index + 1}</div>
+              </div>
             ))}
           </div>
         </div>
